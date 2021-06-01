077574821b2d4221b517cdd534b50bda757fe332
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2021-05-31 20:37:08 -0400

    DSAlgosDotNet5 Init

---
+++ DSAlgosDotNet5/DSAlgosDotNet5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DSAlgosDotNet5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
