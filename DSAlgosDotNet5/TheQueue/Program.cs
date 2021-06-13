b9bbafa3dde17c8327ce72ede3ea24466a484500
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2021-06-13 14:27:36 -0400

    .NET 5.0 Queue Implementation

--- DSAlgosDotNet5/TheQueue/Program.cs
+++ DSAlgosDotNet5/TheQueue/Program.cs
@@ -14,4 +14,68 @@ namespace TheQueue
             Console.WriteLine("Hello World!");
         }
     }
+
+    public class MyQueue
+    {
+        private int maxSize;
+        private long[] myQueue;
+        private int front;
+        private int rear;
+        private int items;
+
+        public MyQueue(int size)
+        {
+            maxSize = size;
+            myQueue = new long[size];
+            front = 0;
+            rear = -1;
+            items = 0;
+        }
+
+        public void Insert(long j)
+        {
+            if (IsFull())
+            {
+                Console.WriteLine("Queue is full");
+            }
+            else
+            {
+                if (rear == maxSize - 1)
+                {
+                    rear = -1;
+                }
+
+                rear++;
+
+                myQueue[rear] = j;
+                items++;
+            }
+        }
+
+        private long Remove()
+        {
+            long temp = myQueue[front];
+            front++;
+            if (front == maxSize)
+            {
+                front = 0;
+            }
+            return temp;
+        }
+
+        public long PeekFront()
+        {
+            return myQueue[front];
+        }
+        
+        public bool IsEmpty()
+        {
+            return items == 0;
+        }
+
+        private bool IsFull()
+        {
+            return items == maxSize;
+        }
+    }
 }
