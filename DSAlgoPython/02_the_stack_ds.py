508bb6d856a00a5c500c9c18ec34ee3ca9445b43
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2021-06-30 21:09:20 -0400

    02_the_stack_ds_v1.1

--- DSAlgoPython/02_the_stack_ds.py
+++ DSAlgoPython/02_the_stack_ds.py
@@ -23,3 +23,17 @@ class Stack:
 
     def __str__(self):
         return str(self.items)
+
+if __name__ == "__main__":
+    s = Stack()
+    print(s)
+    print(s.is_empty())
+    s.push(3)
+    print(s)
+    s.push(7)
+    s.push(5)
+    print(s)
+    print(s.pop())
+    print(s)
+    print(s.peek())
+    print(s.size())
