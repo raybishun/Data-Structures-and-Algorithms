d9e0176242d235cc534ff641353346beaa9ba381
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2021-07-02 21:54:17 -0400

    read_maze

---
+++ DSAlgoPython/read_maze.py
@@ -0,0 +1,13 @@
+def read_maze(file_name):
+    try:
+        with open(file_name) as fh:
+            maze = [[char for char in line.strip("\n")] for line in fh]
+            num_cols_top_row = len(maze[0])
+            for row in maze:
+                if len(row) != num_cols_top_row:
+                    print("The maze is not rectangular.")
+                    raise SystemExit
+            return maze
+    except OSError:
+        print("There is a problem with the file you selected.")
+        raise SystemExit
